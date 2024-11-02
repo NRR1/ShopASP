5e2d2c6108f0782e13f75ca7e6e0b65aa02c6cf1
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-O0VQQ4U>
2024-11-03 00:00:38 +0300

    CRUD for role and future

---
+++ ShopASP.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using ShopASP.Domain.Entities;
+using ShopASP.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopASP.Infrastructure.Repositories
+{
+    public class UserRepository : GenericInterface<User>
+    {
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<User> GetByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task CreateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task VerifyAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
