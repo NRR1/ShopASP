dd20eab5698befbe057cbba8659d5cf9fe54fb82
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-O0VQQ4U>
2024-11-02 23:34:00 +0300

    Определение generic интерфейса

---
+++ ShopASP.Domain/Interfaces/GenericInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopASP.Domain.Interfaces
+{
+    public interface GenericInterface<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIDAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task VerifyAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
