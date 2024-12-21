eb641b690e8e2aef5561d669fba0cafca866b7cf
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-O0VQQ4U>
2024-12-21 16:02:36 +0300

    ProductController

--- ShopASP.Web/Controllers/ProductController.cs
+++ ShopASP.Web/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using ShopASP.Application.DTO;
 using ShopASP.Application.Interface;
 using ShopASP.Web.Models;
 
@@ -18,6 +20,101 @@ namespace ShopASP.Web.Controllers
             return View(vm);
         }
 
+        public async Task<IActionResult> Details(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            ProductDTO product = await service.GetByID(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            ProductViewModel vm = ProductViewModel.FromDTO(product);
+            return View(vm);
+        }
 
+        [HttpGet]
+        public IActionResult Create() => View(new ProductViewModel());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Cost,Quantity")] ProductViewModel vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            ProductDTO product = vm.ToDTO();
+            await service.Create(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var product = await service.GetByID(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            var vm = ProductViewModel.FromDTO(product);
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Desctiption,Cost,Quantity")] ProductViewModel vm)
+        {
+            if(id != vm.ID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                ProductDTO dto = vm.ToDTO();
+                await service.Update(dto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+
+
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var product = await service.GetByID(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost("Delete/{id}")]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConf(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var dProd = await service.GetByID(id);
+            if(dProd == null)
+            {
+                return NotFound();
+            }
+            await service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
-}
+} 
